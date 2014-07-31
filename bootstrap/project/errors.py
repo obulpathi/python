7e10351dae74d8033837d0e22cc8696605c0dd24
Obulapathi N Challa <obulpathi@gmail.com>
2014-07-31 13:46:33 -0400

    Bootstrap

---
+++ bootstrap/project/errors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module contains the set of project's exceptions
+
+:copyright: (c) 2012 by Firstname Lastname.
+:license: ISC, see LICENSE for more details.
+
+"""
+
+
+class ProjectError(Exception):
+    """This is an ambiguous error that occured."""
+    pass
