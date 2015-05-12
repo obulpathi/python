aa9e35c1d022e9c844acc7befca414814064e49c
Obulpathi <obulapathi.challa@rackspace.com>
2015-05-12 14:54:08 -0400

    Memory management in Python

---
+++ memory/memory.py
@@ -0,0 +1,18 @@
+import gc
+from memory_profiler import profile
+
+@profile(precision=3)
+def my_func():
+    gc.set_debug(gc.DEBUG_LEAK)
+    a = [1] * (10 ** 7)
+    b = [1] * (10 ** 5)
+
+    del a
+    del b
+    gc.collect()
+    print("Garbage: ".format(gc.garbage))
+
+print("Garbage collection enabled: {}".format(gc.isenabled()))
+my_func()
+my_func()
+my_func()
